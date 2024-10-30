1c148fbcf4dcf86a691ea718e2773179fe6d86c2
Priya-TTN <132041935+Priya-TTN@users.noreply.github.com>
2024-10-30 10:44:02 +0530

    Create MethodReference.java

---
+++ javaPractice/src/thread/MethodReference.java
@@ -0,0 +1,13 @@
+import java.util.function.Function;
+
+public class MethodReference {
+    public static Integer square(Integer number) {
+        return number * number;
+    }
+  
+public static void main(String[] args) {
+        Function<Integer, Integer> squareFunction = MethodReference::square;
+        Integer result = squareFunction.apply(5);
+        System.out.println(result); 
+    }
+}
