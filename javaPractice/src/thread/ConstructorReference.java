a23123068b7cd07eb0dad6b19f997c66f55a30e6
Priya-TTN <132041935+Priya-TTN@users.noreply.github.com>
2024-11-08 21:16:46 +0530

    Update ConstructorReference.java

--- javaPractice/src/thread/ConstructorReference.java
+++ javaPractice/src/thread/ConstructorReference.java
@@ -1,5 +1,5 @@
+package thread;
 import java.util.function.Function;
-
 public class ConstructorReference {
     public static void main(String[] args) {
         Function<String, Person> personFactory = Person::new;
